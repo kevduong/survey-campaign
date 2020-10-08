064836ab8e32ab7f819a146ed4a4a9585ab91006
kevduong <duong.kev@gmail.com>
2020-10-07 22:16:23 -0700

    Setup Oauth

--- index.js
+++ index.js
@@ -6,17 +6,25 @@ const keys = require('./config/keys');
 const app = express();
 
 passport.use(
-  new GoogleStrategoy(
+  new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     }, 
-      accessToken => {
-        console.log(accessToken);
+      (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile', profile);
     }
   )
 );
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT);
