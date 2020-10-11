07d5994dc432a504f76b6cc5745b5347995b04b5
kevduong <duong.kev@gmail.com>
2020-10-11 15:22:48 -0700

    finish auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,16 @@
 const passport = require('passport');
 
 module.exports = app => {
-  app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-  }));
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
-  applicationCache.get('/api/logout', (req, res) => {
+  app.get('/api/logout', (req, res) => {
     req.logout();
     res.send(req.user);
   });
