4211ac9cbe0536b5a8027974ca0cb25110b2f31b
kevduong <duong.kev@gmail.com>
2020-10-14 22:04:41 -0700

    redux thunk

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
-const Header = () => <h2>Header</h2>
+import Header from './Header'
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-  return (
-    <div>
-     <BrowserRouter>
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser()
+  }
+
+  render() {
+    return (
       <div>
-        <Route path="/" component={Landing}/>
+       <BrowserRouter>
+        <div>
+          <Header/>
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/surveys" component={Dashboard}/>
+          <Route path="/surveys/new" component={SurveyNew}/>
+        </div>
+       </BrowserRouter>
       </div>
-     </BrowserRouter>
-    </div>
-  )
+    )
+  }
 }
 
-export default App
+export default connect(null, actions)(App)
---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="/" className="left brand-logo">Survey Campaign</a>
+          <ul className="right">
+            <li>
+              <a href="/auth/google">Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header
