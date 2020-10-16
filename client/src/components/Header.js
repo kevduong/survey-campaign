d9465449bed8bc74b0b8d52e6a08436ae4bd5279
kevduong <duong.kev@gmail.com>
2020-10-15 21:26:51 -0700

    authReducer return values

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,25 @@
+import { connect } from 'react-redux'
 import React, { Component } from 'react'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Still deciding'      
+      case false:
+        return 'im logged out'
+      default:
+        return 'im logged in'
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="/" className="left brand-logo">Survey Campaign</a>
           <ul className="right">
-            <li>
-              <a href="/auth/google">Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -17,4 +27,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
